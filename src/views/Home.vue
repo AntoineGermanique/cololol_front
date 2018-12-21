a11da351fb1f5c6482560d28c32b6b2a5422bd04
antoine <you@example.com>
2018-12-21 12:12:18 +0100

    feat(): start adding create ad and geocoding/Gplaces

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <button>
+      <router-link to="/search">{{ $t("common.search") }}</router-link>
+    </button>
+    <button>
+      <router-link to="/create">{{ $t("common.createAdd") }}</router-link>
+    </button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
