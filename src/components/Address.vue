7f89ed0b6c96af48ffa8f705d217bf1a683a3618
antoine <you@example.com>
2018-12-28 11:54:29 +0100

    feat():starting form

---
+++ src/components/Address.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>{{ $t("common.locality") }} : {{addressDetails.locality}}</div>
+    <div>{{ $t("common.postal_code") }} : {{addressDetails.postal_code}}</div>
+    <div>{{ $t("common.route") }} : {{addressDetails.route}}</div>
+    <div>{{ $t("common.street_number") }} : {{addressDetails.street_number}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Address",
+  props: ["address"],
+  data: function() {
+    return {
+      addressDetails: this.address
+    };
+  }
+};
+</script>
--- src/components/CreateAdd.vue
+++ src/components/CreateAdd.vue
@@ -1,29 +1,60 @@
 <template>
-  <!-- <vue-google-autocomplete
-    ref="address"
-    id="map"
-    class="form-control"
-    placeholder="Please type your address"
-    v-on:placechanged="getAddressData"
-    country="sg"
-  ></vue-google-autocomplete>-->
-  <vue-google-autocomplete
-    id="map"
-    class="form-control"
-    placeholder="Start typing"
-    v-on:placechanged="getAddressData"
-  ></vue-google-autocomplete>
+  <div>
+    <form>
+      <vue-google-autocomplete
+        id="map"
+        class="form-control"
+        placeholder="Start typing"
+        v-on:placechanged="getAddressData"
+      ></vue-google-autocomplete>
+      <Address v-bind:address="address"></Address>
+      <label>
+        {{$t("common.monthlyRent")}}
+        <input v-model="monthlyRent" type="number">
+      </label>
+      <label>
+        {{$t("common.extra")}}
+        <input v-model="extra" type="number">
+      </label>
+      <span>{{$t("common.total")}} :{{parseFloat(monthlyRent)+parseFloat(extra)}}</span>
+      <label>
+        {{$t("common.roomatesNumber")}}
+        <input v-model="roomatesNumber" type="number">
+      </label>
+      <label>
+        {{$t("common.description")}}
+        <textarea v-model="description"></textarea>
+      </label>
+      <label>
+        {{$t("common.description")}}
+        <input
+          ref="myPics"
+          class="custom-file-input"
+          @change="previewPics"
+          multiple
+          type="file"
+          accept=".jpg, .png"
+          size="1000"
+        >
+      </label>
+      <div v-for="(pic, index) in pics" v-bind:key="index">tada {{pic}}</div>
+    </form>
+  </div>
 </template>
 <script>
 import VueGoogleAutocomplete from "vue-google-autocomplete";
+import Address from "@/components/Address.vue";
 
 export default {
-  components: { VueGoogleAutocomplete },
-  data: function() {
-    return {
-      address: ""
-    };
-  },
+  components: { VueGoogleAutocomplete, Address },
+  data: () => ({
+    address: "",
+    monthlyRent: 0,
+    extra: 0,
+    roomatesNumber: 0,
+    descripton: "",
+    pics: []
+  }),
 
   mounted() {
     // To demonstrate functionality of exposed component functions
@@ -40,6 +71,9 @@ export default {
      */
     getAddressData: function(addressData, placeResultData, id) {
       this.address = addressData;
+    },
+    previewPics() {
+      this.pics = this.pics.concat(this.$refs.myPics.files);
     }
   }
 };
